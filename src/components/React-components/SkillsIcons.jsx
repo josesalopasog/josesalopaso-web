0213553336b99805dd9283e0d23afeb22737eac3
josesalopasog <josesalopaso.13@gmail.com>
2025-01-25 20:18:08 -0500

    Fixing sizing and responsive design in Skills Education and Footer

--- src/components/React-components/SkillsIcons.jsx
+++ src/components/React-components/SkillsIcons.jsx
@@ -55,122 +55,122 @@ const SkillsIcons = ({
   return (
     <div className="flex flex-wrap gap-1 items-center justify-center w-full cursor-pointer">
       {showReactIcon && (
-        <div className="skill-icon">
-          <ReactIcon className="w-5 h-5" />
+        <div className="skill-container">
+          <ReactIcon className="skill-icon" />
           <p>React</p>
         </div>
       )}
       {showViteIcon && (
-        <div className="skill-icon">
-          <ViteJsIcon className="w-5 h-5" />
+        <div className="skill-container">
+          <ViteJsIcon className="skill-icon" />
           <p>Vite.js</p>
         </div>
       )}
       {showAstroIcon && (
-        <div className="skill-icon">
-          <AstroIcon className="w-5 h-5" />
+        <div className="skill-container">
+          <AstroIcon className="skill-icon" />
           <p>Astro</p>
         </div>
       )}      
       {showTailwindIcon && (
-        <div className="skill-icon">
-          <TailwindCssIcon className="w-5 h-5" />
+        <div className="skill-container">
+          <TailwindCssIcon className="skill-icon" />
           <p>TailwindCSS</p>
         </div>
       )}
       {showJSIcon && (
-        <div className="skill-icon">
-          <JavascriptIcon className="w-5 h-5" />
+        <div className="skill-container">
+          <JavascriptIcon className="skill-icon" />
           <p>Javascript</p>
         </div>
       )}
       {showHTMLIcon && (
-        <div className="skill-icon">
-          <Html5Icon className="w-5 h-5" />
+        <div className="skill-container">
+          <Html5Icon className="skill-icon" />
           <p>HTML</p>
         </div>
       )}
       {showCSSIcon && (
-        <div className="skill-icon">
-          <CssIcon className="w-5 h-5" />
+        <div className="skill-container">
+          <CssIcon className="skill-icon" />
           <p>CSS</p>
         </div>
       )}
       {showPythonIcon && (
-        <div className="skill-icon">
-          <PythonIcon className="w-5 h-5" />
+        <div className="skill-container">
+          <PythonIcon className="skill-icon" />
           <p>Python</p>
         </div>
       )}
       {showExcelIcon && (
-        <div className="skill-icon">
-          <ExcelIcon className="w-5 h-5" />
+        <div className="skill-container">
+          <ExcelIcon className="skill-icon" />
           <p>Excel</p>
         </div>
       )}
       {showSapIcon && (
-        <div className="skill-icon">
-          <SapIcon className="w-5 h-5" />
+        <div className="skill-container">
+          <SapIcon className="skill-icon" />
           <p>SAP</p>
         </div>
       )}
       {showJupyterIcon && (
-        <div className="skill-icon">
-          <JupyterIcon className="w-5 h-5" />
+        <div className="skill-container">
+          <JupyterIcon className="skill-icon" />
           <p>Jupyter NB</p>
         </div>
       )}
       {showGitIcon && (
-        <div className="skill-icon">
-          <GitIcon className="w-5 h-5" />
+        <div className="skill-container">
+          <GitIcon className="skill-icon" />
           <p>Git</p>
         </div>
       )}
       {showGithubIcon && (
-        <div className="skill-icon">
-          <GithubIcon className="w-5 h-5 fill-black dark:fill-white" />
+        <div className="skill-container">
+          <GithubIcon className="skill-icon fill-black dark:fill-white" />
           <p>Github</p>
         </div>
       )}
       {showVsCodeIcon && (
-        <div className="skill-icon">
-          <VsCodeIcon className="w-5 h-5" />
+        <div className="skill-container">
+          <VsCodeIcon className="skill-icon" />
           <p>VS Code</p>
         </div>
       )}
       {showPowerBIIcon && (
-        <div className="skill-icon">
-          <PowerBiIcon className="w-5 h-5" />
+        <div className="skill-container">
+          <PowerBiIcon className="skill-icon" />
           <p>Power BI</p>
         </div>
       )}
       {showLinuxIcon && (
-        <div className="skill-icon">
-          <img src={linuxIcon.src} alt="linux-icon" />
+        <div className="skill-container">
+          <img className="skill-icon" src={linuxIcon.src} alt="linux-icon" />
           <p>Linux</p>
         </div>
       )}
       {showPostgreSQLIcon && (
-        <div className="skill-icon">
-          <PostgreSqlICon className="w-5 h-5" />
+        <div className="skill-container">
+          <PostgreSqlICon className="skill-icon" />
           <p>PostgreSQL</p>
         </div>
       )}
       {showMySQLIcon && (
-        <div className="skill-icon">
-          <MySqlIcon className="w-5 h-5" />
+        <div className="skill-container">
+          <MySqlIcon className="skill-icon" />
           <p>MySQL</p>
         </div>
       )}
       {showNodeJSIcon && (
-        <div className="skill-icon">
-          <img src={nodejsIcon.src} alt="nodejs-icon" />
+        <div className="skill-container">
+          <img className="skill-icon" src={nodejsIcon.src} alt="nodejs-icon" />
           <p>Node.js</p>
         </div>
       )}
       {showExpressJSIcon && (
-        <div className="skill-icon">
-          <ExpressJsIcon className="w-5 h-5 fill-black dark:fill-white" />
+        <div className="skill-container">
+          <ExpressJsIcon className="skill-icon fill-black dark:fill-white" />
           <p>Express.js</p>
         </div>
       )}
